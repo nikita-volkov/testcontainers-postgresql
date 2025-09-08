506cd093d82e721aaba7843678254d267669019d
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-09-08 15:18:07 +0300

    Correct the docs

--- src/library/TestcontainersPostgresql.hs
+++ src/library/TestcontainersPostgresql.hs
@@ -20,7 +20,7 @@ setup config = do
   container <- TestContainers.run (Configs.Config.toContainerRequest config)
   pure $ TestContainers.containerAddress container 5432
 
--- | Run a session on a PostgreSQL container on the scope of a host and a port.
+-- | Run a session on a PostgreSQL container in the scope of a host and a port.
 run :: Configs.Config.Config -> ((Text, Int) -> IO ()) -> IO ()
 run config = do
   TestContainers.Hspec.withContainers (setup config)
