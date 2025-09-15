4ede1d63c891c2916079378c865198cbfb70cafb
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-09-15 14:41:41 +0300

    Add more distros

--- src/library/TestcontainersPostgresql/Configs/Distro.hs
+++ src/library/TestcontainersPostgresql/Configs/Distro.hs
@@ -6,7 +6,8 @@ import qualified TestContainers
 import Prelude
 
 data Distro
-  = Distro9
+  = Distro8
+  | Distro9
   | Distro10
   | Distro11
   | Distro12
@@ -15,10 +16,12 @@ data Distro
   | Distro15
   | Distro16
   | Distro17
+  | Distro18
   deriving stock (Show, Eq)
 
 toTag :: Distro -> Text
 toTag = \case
+  Distro8 -> "postgres:8"
   Distro9 -> "postgres:9"
   Distro10 -> "postgres:10"
   Distro11 -> "postgres:11"
@@ -28,6 +31,7 @@ toTag = \case
   Distro15 -> "postgres:15"
   Distro16 -> "postgres:16"
   Distro17 -> "postgres:17"
+  Distro18 -> "postgres:18"
 
 toToImage :: Distro -> TestContainers.ToImage
 toToImage = TestContainers.fromTag . toTag
